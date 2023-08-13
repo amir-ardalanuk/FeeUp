cc392837a4292fdf6e8b341d48ff2af80fea9c96
Amir Ardalani <amir.ardalanuk@gmail.com>
2023-08-13 14:21:08 +0330

    feat: create `News` and `Source`entites

---
+++ FeeUp/LocalPackages/Packages/Sources/Domain/Entity/News.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+// MARK: - News
+
+public struct News: Codable, Hashable {
+    public let source: Source
+    public let author: String?
+    public let title, description: String
+    public let url: String
+    public let urlToImage: String?
+    public let publishedAt: Date
+    public let content: String
+
+    init(source: Source, author: String? = nil, title: String, description: String, url: String, urlToImage: String? = nil, publishedAt: Date, content: String) {
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+    }
+}
