844666ead224245229683744f0d04b4d7acdd512
Amir Ardalani <amir.ardalanuk@gmail.com>
2023-08-13 18:28:46 +0330

    implement `ResourceLoading` for local json  f

---
+++ FeeUp/LocalPackages/Packages/Sources/API/ResourceLoading.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+enum ResourceLoadingError: Error {
+    case thereIsNotSuchFile
+}
+public protocol ResourceLoading {
+    func data(forResource: String?, withExtension: String?) throws -> Data?
+}
+
+extension Bundle: ResourceLoading {
+    public func data(forResource resource: String?, withExtension: String?) throws -> Data? {
+        guard let url = url(forResource: resource, withExtension: withExtension) else {
+            throw ResourceLoadingError.thereIsNotSuchFile
+        }
+        do {
+            let data = try Data(contentsOf: url)
+            return data
+        } catch {
+            throw error
+        }
+    }
+}
