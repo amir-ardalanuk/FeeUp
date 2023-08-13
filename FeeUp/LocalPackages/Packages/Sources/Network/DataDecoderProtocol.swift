6682575d5a8d3db201659713433915618276c344
Amir Ardalani <amir.ardalanuk@gmail.com>
2023-08-13 14:56:32 +0330

    feat: create a wrapper protocol for api request

---
+++ FeeUp/LocalPackages/Packages/Sources/Network/DataDecoderProtocol.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+/// A protocol defining the contract for a data decoder.
+public protocol DataDecoderProtocol {
+    /// Decodes the given data into an instance of the specified type.
+    /// - Parameters:
+    ///   - data: The data to decode.
+    ///   - type: The type of the object to decode into.
+    /// - Returns: The decoded object of the specified type.
+    /// - Throws: An error if decoding fails.
+    func decode<T: Decodable>(_ data: Data, type: T.Type) throws -> T
+}
+
+/// A data decoder implementation for JSON data.
+public final class JsonDecoder: DataDecoderProtocol {
+    let decoder: JSONDecoder
+
+    /// Initializes a new instance of the JSON decoder with an optional custom JSON decoder.
+    /// - Parameter decoder: The JSON decoder to use. If not provided, a default JSON decoder is used.
+    init(decoder: JSONDecoder = .init()) {
+        self.decoder = decoder
+    }
+
+    public func decode<T>(_ data: Data, type: T.Type) throws -> T where T: Decodable {
+        return try decoder.decode(T.self, from: data)
+    }
+}
---
+++ FeeUp/LocalPackages/Packages/Sources/Network/RequestManagerProtocol.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+/// A protocol defining the contract for a request manager.
+public protocol RequestManagerProtocol {
+    /// Performs the specified request and returns the decoded response object.
+    /// - Parameter request: The request to perform.
+    /// - Returns: The decoded response object.
+    /// - Throws: An error if the request fails or the response cannot be decoded.
+    ///
+    ///
+    @discardableResult
+    func perform<T: Decodable>(_ request: RequestProtocol) async throws -> T
+}
+
+/// A request manager implementation for performing network requests and decoding the response.
+public final class RequestManager: RequestManagerProtocol {
+    let apiManager: APIManagerProtocol
+    let decoder: DataDecoderProtocol
+    /// Initializes a new instance of the request manager with an optional custom API manager.
+    /// - Parameter apiManager: The API manager to use. If not provided, a default API manager is used.
+    init(apiManager: APIManagerProtocol = APIManager(), decoder: DataDecoderProtocol = JsonDecoder()) {
+        self.apiManager = apiManager
+        self.decoder = decoder
+    }
+
+    @discardableResult
+    public func perform<T: Decodable>(_ request: RequestProtocol) async throws -> T {
+        do {
+            let data = try await apiManager.perform(request)
+            return try decoder.decode(data, type: T.self)
+        } catch {
+            if let network = error as? NetworkError {
+                switch network {
+                case let .invalidServerResponse(data):
+                    let serverError = try decoder.decode(data ?? .init(), type: ServerError.self)
+                    throw NetworkError.ServerError(serverError)
+                default:
+                    throw error
+                }
+            } else {
+                throw error
+            }
+        }
+    }
+}
