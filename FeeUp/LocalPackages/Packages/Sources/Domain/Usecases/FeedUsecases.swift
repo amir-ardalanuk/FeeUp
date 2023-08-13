6578a2bf5bd484d53f3014e82e71992700f57e5f
Amir Ardalani <amir.ardalanuk@gmail.com>
2023-08-13 14:32:48 +0330

    feat: create `FeedUsecases` from  requirement

---
+++ FeeUp/LocalPackages/Packages/Sources/Domain/Usecases/FeedUsecases.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+protocol FeedUsecases {
+    func fetchLatest(withSearch: String?, page: Int, pageSize: Int) async throws -> [News]
+    func filter(country: String?, category: String?) async throws -> [News]
+    func bookmark(news: News) async throws
+    func removeBookmark(newsId: String) async throws
+}
+
+extension FeedUsecases {
+    func fetchLatest(withSearch search: String? = nil, page: Int = 1, pageSize: Int = 100) async throws -> [News] {
+        try await fetchLatest(withSearch: search, page: page, pageSize: pageSize)
+    }
+
+    func filter(country: String? = nil, category: String? = nil) async throws -> [News] {
+        try await filter(country: country, category: category)
+    }
+
+}
