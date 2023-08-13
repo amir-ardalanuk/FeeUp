95fb8ce7eaad4cc3fad3d8d620130a5f4cfad15c
Amir Ardalani <amir.ardalanuk@gmail.com>
2023-08-13 13:18:12 +0330

    feat: create a basic iOS project

---
+++ FeeUp/FeeUp/FeeUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeeUpApp.swift
+//  FeeUp
+//
+//  Created by Amir Ardalan on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct FeeUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
