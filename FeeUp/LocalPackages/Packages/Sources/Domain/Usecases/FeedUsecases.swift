eb7fe850e63d3c6c48da366db8cf8429d7ec23e9
Amir Ardalani <amir.ardalanuk@gmail.com>
2023-08-13 18:28:02 +0330

    move `FeedQuery` to Domain module
    and change FeedUsecases methods and parameters

--- FeeUp/LocalPackages/Packages/Sources/Domain/Usecases/FeedUsecases.swift
+++ FeeUp/LocalPackages/Packages/Sources/Domain/Usecases/FeedUsecases.swift
@@ -1,23 +1,11 @@
 import Foundation
 
 public protocol FeedUsecases {
-    func fetchLatest(withSearch: String?, page: Int, pageSize: Int) async throws -> [News]
-    func filter(country: String?, category: String?) async throws -> [News]
-    func countries() async throws -> [FeedCountries]
+    func fetchLatest(query: FeedQuery) async throws -> [News]
+    func countries() async throws -> FeedCountries
 }
 
 public protocol FeedBookmarkUsecases {
     func bookmark(news: News) async throws
     func removeBookmark(newsId: String) async throws
-}
-
-extension FeedUsecases {
-    func fetchLatest(withSearch search: String? = nil, page: Int = 1, pageSize: Int = 100) async throws -> [News] {
-        try await fetchLatest(withSearch: search, page: page, pageSize: pageSize)
-    }
-
-    func filter(country: String? = nil, category: String? = nil) async throws -> [News] {
-        try await filter(country: country, category: category)
-    }
-
 }
