614ac235f10e5c053aee3f1f99e697dbad0d31df
Amir Ardalani <amir.ardalanuk@gmail.com>
2023-08-13 17:54:13 +0330

    add `FeedCategory`

---
+++ FeeUp/LocalPackages/Packages/Sources/Domain/Entity/FeedCategories.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public enum FeedCategory: String, CaseIterable{
+    case business
+    case entertainment
+    case general
+    case health
+    case science
+    case sports
+    case technology
+}
